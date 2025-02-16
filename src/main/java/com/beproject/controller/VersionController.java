8c4018e247d633fac0bc1c29e4aed5b89f91c228
Arthur <fonichkin.arthur@gmail.com>
2025-02-16 19:48:36 +0300

    add argocd

--- src/main/java/com/beproject/controller/VersionController.java
+++ src/main/java/com/beproject/controller/VersionController.java
@@ -14,6 +14,6 @@ public class VersionController {
 
     @GetMapping()
     public ResponseEntity<String> getVersion() {
-        return ResponseEntity.ok("1.1.2");
+        return ResponseEntity.ok("1.5");
     }
 }
