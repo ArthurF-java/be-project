9b7f77a1c1458180aebf39a49007b42119bede46
Arthur <fonichkin.arthur@gmail.com>
2025-02-17 13:39:51 +0300

    add notifications and ping

--- src/main/java/com/beproject/controller/VersionController.java
+++ src/main/java/com/beproject/controller/VersionController.java
@@ -14,6 +14,6 @@ public class VersionController {
 
     @GetMapping()
     public ResponseEntity<String> getVersion() {
-        return ResponseEntity.ok("2.2.0");
+        return ResponseEntity.ok("3.0.0");
     }
 }
