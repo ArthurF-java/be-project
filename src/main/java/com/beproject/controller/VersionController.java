e6b0ca539cdbed47af5f5f72d62e661ccd429ca6
Arthur <fonichkin.arthur@gmail.com>
2025-02-20 12:58:39 +0300

    v6.1.0

--- src/main/java/com/beproject/controller/VersionController.java
+++ src/main/java/com/beproject/controller/VersionController.java
@@ -15,6 +15,6 @@ public class VersionController {
 
     @GetMapping()
     public ResponseEntity<VersionResponse> getVersion() {
-        return ResponseEntity.ok(new VersionResponse("6.1.0"));
+        return ResponseEntity.ok(new VersionResponse("6.5.0"));
     }
 }
